1b35d240bc9a29c44e60613885f9c7d71331a984
Ravindra Bhartiya <ravibh@microsoft.com>
2026-01-28 15:51:36 -0800

    Add Application Insights for visitor tracking

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using AILearningApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Application Insights telemetry for visitor tracking
+builder.Services.AddApplicationInsightsTelemetry();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
